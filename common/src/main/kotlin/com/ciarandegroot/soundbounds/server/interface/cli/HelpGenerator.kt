e4eaaa8597cd83fc1fd49a83d1d514e8fb547bb5
Ciaran De Groot <ciaran.deg@gmail.com>
2021-05-23 17:04:23 -0600

    Improve HelpGenerator algorithm
    HelpGenerator does a much cleaner tree traversal now, by
    converting the entire tree to a tree of HelpTreeNodes and
    building documentation from that rather than attempting
    to do it all in one step.

--- common/src/main/kotlin/com/ciarandegroot/soundbounds/server/interface/cli/HelpGenerator.kt
+++ common/src/main/kotlin/com/ciarandegroot/soundbounds/server/interface/cli/HelpGenerator.kt
@@ -1,52 +1,26 @@
 package com.ciarandegroot.soundbounds.server.`interface`.cli
 
-import com.ciarandegroot.soundbounds.common.command.ArgNodeData
 import com.ciarandegroot.soundbounds.common.command.CommandNode
-import com.ciarandegroot.soundbounds.common.command.LiteralNodeData
 import net.minecraft.text.LiteralText
 import net.minecraft.text.MutableText
-import java.security.InvalidParameterException
 
 object HelpGenerator {
     fun generate(root: CommandNode): List<MutableText> {
-        val descriptions: MutableList<Pair<String, String?>> = MutableList(0) { Pair("", "") }
-        parse(root, descriptions)
-        return formatDescriptions(descriptions)
+        return generate(HelpTreeNode(root))
     }
 
-    private fun parse(
-        n: CommandNode,
-        store: MutableList<Pair<String, String?>>,
-        cmd: String = "",
-        parentNodeHadWork: Boolean = false
-    ) {
-        var swap = "$cmd "
-        swap += generateNodeText(n, parentNodeHadWork)
-
-        when (n.data) {
-            is LiteralNodeData -> store.add(Pair(swap, n.data.description))
-            is ArgNodeData<*, *> -> store[store.lastIndex] = Pair(swap, store.last().second)
+    private fun generate(node: HelpTreeNode, entries: MutableList<MutableText> = mutableListOf()): List<MutableText> {
+        if (node.description != null) {
+            entries.add(
+                LiteralText("/${node.command}").formatted(Colors.Help.COMMAND) +
+                        LiteralText(" - ").formatted(Colors.SEPARATOR) +
+                        LiteralText(node.description).formatted(Colors.Help.DESCRIPTION)
+            )
         }
 
-        for (c in n.children) parse(c, store, swap, n.data.work != null)
+        for (n in node.children) generate(n, entries)
+        return entries
     }
 
-    private fun generateNodeText(n: CommandNode, isOptional: Boolean): String =
-        when (n.data) {
-            is LiteralNodeData -> n.data.literal
-            is ArgNodeData<*, *> -> {
-                val br = if (isOptional) "[]" else "<>"
-                "${br[0]}${n.data.arg.name}${br[1]}"
-            }
-            else -> throw InvalidParameterException()
-        }
-
-    private fun formatDescriptions(descriptions: MutableList<Pair<String, String?>>) =
-        descriptions.filter { entry -> entry.second != null }.map { l ->
-            LiteralText("/${l.first}").formatted(Colors.Help.COMMAND) +
-                    LiteralText(" - ").formatted(Colors.SEPARATOR) +
-                    LiteralText(l.second).formatted(Colors.Help.DESCRIPTION)
-        }
+    private operator fun MutableText.plus(text: MutableText) = append(text)
 }
-
-private operator fun MutableText.plus(text: MutableText) = append(text)
