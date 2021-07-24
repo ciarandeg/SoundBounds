403bdc554802438c1d19347d611a87bb9d670ee4
Ciaran De Groot <ciaran.deg@gmail.com>
2021-07-23 22:53:12 -0600

    Make composer promo optional

--- common/src/main/kotlin/com/ciarandg/soundbounds/common/metadata/JsonMeta.kt
+++ common/src/main/kotlin/com/ciarandg/soundbounds/common/metadata/JsonMeta.kt
@@ -3,10 +3,12 @@ package com.ciarandg.soundbounds.common.metadata
 import java.net.URL
 
 data class JsonMeta(
-    val composers: Map<String, URL> = emptyMap(),
+    val composers: Map<String, JsonComposerMeta> = emptyMap(),
     val groups: Map<String, List<String>> = emptyMap(),
     val songs: Map<String, JsonSongMeta> = emptyMap()
 )
+
+data class JsonComposerMeta(val promo: URL?)
 
 data class JsonSongMeta(
     val title: String,
