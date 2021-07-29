a724ac7498087c3c062d47c0c6b4faa92119b118
Ciaran De Groot <ciaran.deg@gmail.com>
2021-07-29 17:31:16 -0600

    Add alias now-playing command for sb now-playing

--- common/src/main/kotlin/com/ciarandg/soundbounds/common/ui/cli/command/SoundBoundsCommand.kt
+++ common/src/main/kotlin/com/ciarandg/soundbounds/common/ui/cli/command/SoundBoundsCommand.kt
@@ -3,10 +3,18 @@ package com.ciarandg.soundbounds.common.ui.cli.command
 import com.ciarandg.soundbounds.common.ui.cli.Assembler
 import com.ciarandg.soundbounds.common.ui.cli.command.nodes.RootNode
 import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import net.minecraft.server.command.ServerCommandSource as Source
 
 object SoundBoundsCommand {
     fun register(dispatcher: CommandDispatcher<Source>) {
         dispatcher.register(Assembler.assembleLiteral(RootNode).requires { it.hasPermissionLevel(4) })
+        dispatcher.register(
+            LiteralArgumentBuilder.literal<Source>("now-playing")
+                .executes { cmd ->
+                    dispatcher.execute("sb now-playing", cmd.source)
+                    0
+                }
+        )
     }
 }
