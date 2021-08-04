33e27f600bc0dd369c74e341d8cdfe5bb9b3cadc
Ciaran De Groot <ciaran.deg@gmail.com>
2021-08-03 19:11:13 -0600

    Catch SongMetaMismatchException on PlaylistPlayer start/tick

--- common/src/main/kotlin/com/ciarandg/soundbounds/client/audio/playlist/PlaylistPlayer.kt
+++ common/src/main/kotlin/com/ciarandg/soundbounds/client/audio/playlist/PlaylistPlayer.kt
@@ -2,6 +2,7 @@ package com.ciarandg.soundbounds.client.audio.playlist
 
 import com.ciarandg.soundbounds.SoundBounds
 import com.ciarandg.soundbounds.client.exceptions.NoMetadataException
+import com.ciarandg.soundbounds.client.exceptions.SongMetaMismatchException
 import com.ciarandg.soundbounds.common.PlaylistType
 
 class PlaylistPlayer(playlist: List<String>, type: PlaylistType, private val queuePersist: Boolean) {
@@ -13,12 +14,16 @@ class PlaylistPlayer(playlist: List<String>, type: PlaylistType, private val que
             state = currentState.start()
         } catch (e: NoMetadataException) {
             SoundBounds.LOGGER.warn("Attempted to start playlist, but there is no client-side metadata available")
+        } catch (e: SongMetaMismatchException) {
+            handleMetaMismatch(e)
         }
         else -> SoundBounds.LOGGER.error("Attempted to start a playlist that already started")
     }
 
-    fun tick() {
+    fun tick() = try {
         state = state.tick()
+    } catch (e: SongMetaMismatchException) {
+        handleMetaMismatch(e)
     }
 
     fun stop() {
@@ -31,4 +36,7 @@ class PlaylistPlayer(playlist: List<String>, type: PlaylistType, private val que
         is PlaylistPlayerPlayingState -> currentState.currentSongID()
         else -> null
     }
+
+    private fun handleMetaMismatch(e: SongMetaMismatchException) =
+        SoundBounds.LOGGER.warn("Attempted to play a song that has no metadata: ${e.songID}")
 }
