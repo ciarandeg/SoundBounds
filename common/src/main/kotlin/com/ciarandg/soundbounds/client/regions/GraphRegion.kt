e401848dabd795672c8621d18f25eed0382a88eb
Ciaran De Groot <ciaran.deg@gmail.com>
2021-10-04 19:26:27 -0600

    Refactor

--- common/src/main/kotlin/com/ciarandg/soundbounds/client/regions/GraphRegion.kt
+++ common/src/main/kotlin/com/ciarandg/soundbounds/client/regions/GraphRegion.kt
@@ -1,65 +1,30 @@
 package com.ciarandg.soundbounds.client.regions
 
-import com.ciarandg.soundbounds.SoundBounds
 import com.ciarandg.soundbounds.client.render.toBox
-import com.google.common.graph.GraphBuilder
-import com.google.common.graph.MutableGraph
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Vec3i
 import java.lang.IllegalStateException
 
 @Suppress("UnstableApiUsage")
-class GraphRegion(blocks: Set<BlockPos>) {
-    private val graph: MutableGraph<BlockPos> = GraphBuilder.undirected().allowsSelfLoops(false).build()
-    init {
-        blocks.forEach {
-            graph.addNode(it)
-            addAllEdges(it)
-        }
-    }
-
-    private fun addAllEdges(node: BlockPos) {
-        val nodes = graph.nodes()
-        if (!nodes.contains(node)) throw IllegalStateException()
-
-        val potentialAdjacencies = listOf(
-            BlockPos(node.x + 1, node.y, node.z),
-            BlockPos(node.x - 1, node.y, node.z),
-            BlockPos(node.x, node.y + 1, node.z),
-            BlockPos(node.x, node.y - 1, node.z),
-            BlockPos(node.x, node.y, node.z + 1),
-            BlockPos(node.x, node.y, node.z - 1)
-        )
-
-        for (a in potentialAdjacencies) {
-            if (nodes.contains(a)) graph.putEdge(node, a)
-        }
-    }
-
+class GraphRegion(private val blocks: Set<BlockPos>) {
     fun getWireframe(): Set<Pair<Vec3i, Vec3i>> {
-        // map nodes to their 12 corresponding physical edges, filter out any that are shared by adjacent nodes
-        val nodes = graph.nodes()
-        val blocksWithEdges = nodes.associateWith { getBlockPhysicalEdges(it) }
+        val blocksWithEdges = blocks.associateWith { getBlockPhysicalEdges(it) }
 
-        SoundBounds.LOGGER.info("EDGE COUNT WITHOUT CULLING: ${blocksWithEdges.size}")
-
-        // val unfiltered = blocksWithEdges.values.flatten().map { Pair(it.vec1, it.vec2) }
-        // return unfiltered.toSet()
-
-        val lines = HashMap<Line, Int>()
+        // Edges on the perimeter are unique to their corresponding block within the region,
+        // so I count the instances of each edge and ditch anything with more than one instance
+        val lines = HashMap<Line3i, Int>()
         for (entry in blocksWithEdges.entries) {
             entry.value.forEach { newLine ->
                 val currentCount = lines.getOrDefault(newLine, 0)
                 lines[newLine] = currentCount + 1
             }
         }
-
-        val filtered = lines.entries.filter { it.value == 1 }.map { it.key }
-        SoundBounds.LOGGER.info("EDGE COUNT AFTER CULLING: ${filtered.size}")
-        return filtered.map { Pair(it.vec1, it.vec2) }.toSet()
+        val filtered = lines.entries.mapNotNull { if (it.value == 1) Pair(it.key.vertex1, it.key.vertex2) else null }
+        return filtered.toSet()
     }
 
-    private fun getBlockPhysicalEdges(block: BlockPos): Set<Line> {
+    private fun getBlockPhysicalEdges(block: BlockPos): Set<Line3i> {
+        // map a block to its 12 corresponding physical edges
         val corners = getBlockCornerPositions(block)
         val edges = corners.flatMapIndexed { index, c ->
             corners.subList(index + 1, corners.size).mapNotNull {
@@ -68,7 +33,7 @@ class GraphRegion(blocks: Set<BlockPos>) {
                     (c.x == it.x && c.y == it.y) ||
                     (c.x == it.x && c.z == it.z) ||
                     (c.y == it.y && c.z == it.z)
-                ) Line(c, it)
+                ) Line3i(c, it)
                 else null
             }
         }.toSet()
@@ -90,20 +55,15 @@ class GraphRegion(blocks: Set<BlockPos>) {
         )
     }
 
-    private class Line(val vec1: Vec3i, val vec2: Vec3i) {
+    private class Line3i(val vertex1: Vec3i, val vertex2: Vec3i) {
+        private val pair1 = Pair(vertex1, vertex2)
+        private val pair2 = Pair(vertex2, vertex1)
+
         override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
-
-            other as Line
-
-            return (vec1 == other.vec1 && vec2 == other.vec2) || (vec1 == other.vec2 && vec2 == other.vec1)
+            if (other !is Line3i) return false
+            val otherPair = Pair(other.vertex1, other.vertex2)
+            return pair1 == otherPair || pair2 == otherPair
         }
-
-        override fun hashCode(): Int {
-            var result = vec1.hashCode()
-            result = 31 * (result + vec2.hashCode())
-            return result
-        }
+        override fun hashCode() = 31 * (pair1.hashCode() + pair2.hashCode())
     }
 }
