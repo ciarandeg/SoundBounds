2cc33ce686dbcab35e3e594fab163f5e2ab97856
Ciaran De Groot <ciaran.deg@gmail.com>
2021-10-17 21:08:42 -0600

    Clean up event logic

--- common/src/main/kotlin/com/ciarandg/soundbounds/client/event/events/BatonMenuEvent.kt
+++ common/src/main/kotlin/com/ciarandg/soundbounds/client/event/events/BatonMenuEvent.kt
@@ -1,24 +1,28 @@
 package com.ciarandg.soundbounds.client.event.events
 
-import com.ciarandg.soundbounds.SoundBounds
 import com.ciarandg.soundbounds.client.ui.radial.BatonMenu
 import com.ciarandg.soundbounds.common.item.IBaton
 import me.shedaniel.architectury.event.events.TickEvent
 import me.shedaniel.architectury.registry.KeyBindings
 import net.minecraft.client.MinecraftClient
+import net.minecraft.util.math.Vec2f
 
 object BatonMenuEvent {
+    private var menu: BatonMenu? = null
+
     fun register() {
         KeyBindings.registerKeyBinding(BatonMenu.binding)
-
         TickEvent.PLAYER_POST.register {
-            val client = MinecraftClient.getInstance()
-            if (
-                BatonMenu.binding.isPressed &&
-                client.player?.isHolding { it is IBaton } == true &&
-                client.currentScreen == null
-            )
-                SoundBounds.LOGGER.info("Opening baton menu ${System.currentTimeMillis()}")
+            menu = if (!isBindingPressed()) null else BatonMenu(getMousePos())
+            menu?.draw()
         }
     }
+
+    private fun isBindingPressed() = with(MinecraftClient.getInstance()) {
+        BatonMenu.binding.isPressed &&
+            player?.isHolding { it is IBaton } == true &&
+            currentScreen == null
+    }
+
+    private fun getMousePos() = with(MinecraftClient.getInstance().mouse) { Vec2f(x.toFloat(), y.toFloat())  }
 }
