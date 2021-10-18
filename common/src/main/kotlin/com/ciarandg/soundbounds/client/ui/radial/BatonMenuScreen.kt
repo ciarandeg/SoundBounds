799f3b6ecff92344a2edb6769662782040a45687
Ciaran De Groot <ciaran.deg@gmail.com>
2021-10-17 23:48:07 -0600

    Draw debug line from center of screen

--- common/src/main/kotlin/com/ciarandg/soundbounds/client/ui/radial/BatonMenuScreen.kt
+++ common/src/main/kotlin/com/ciarandg/soundbounds/client/ui/radial/BatonMenuScreen.kt
@@ -3,9 +3,10 @@ package com.ciarandg.soundbounds.client.ui.radial
 import com.ciarandg.soundbounds.SoundBounds
 import com.ciarandg.soundbounds.client.ui.radial.buttons.RedoButton
 import com.ciarandg.soundbounds.client.ui.radial.buttons.UndoButton
+import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.screen.Screen
-import net.minecraft.client.gui.widget.AbstractPressableButtonWidget
 import net.minecraft.client.options.KeyBinding
+import net.minecraft.client.render.RenderLayer
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.text.LiteralText
 import net.minecraft.util.math.Vec2f
@@ -21,7 +22,16 @@ class BatonMenuScreen : Screen(LiteralText("Bounds Baton Menu")) {
     override fun render(matrices: MatrixStack, mouseX: Int, mouseY: Int, delta: Float) {
         renderBackground(matrices)
         super.render(matrices, mouseX, mouseY, delta)
+        if (SHOW_DEBUG_LINE) renderDebugLine(mouseX, mouseY)
     }
+
+    private fun renderDebugLine(mouseX: Int, mouseY: Int) =
+        with(MinecraftClient.getInstance().bufferBuilders.entityVertexConsumers) {
+            val buffer = getBuffer(RenderLayer.getLines())
+            buffer.vertex(width.toDouble() / 2, height.toDouble() / 2, 0.0).color(1.0f, 1.0f, 1.0f, 1.0f).next()
+            buffer.vertex(mouseX.toDouble(), mouseY.toDouble(), 0.0).color(1.0f, 1.0f, 1.0f, 1.0f).next()
+            draw()
+        }
 
     fun click(mousePos: Vec2f) {
         getHoveredButton(origin, mousePos).onClick()
@@ -32,6 +42,7 @@ class BatonMenuScreen : Screen(LiteralText("Bounds Baton Menu")) {
     }
 
     companion object {
+        const val SHOW_DEBUG_LINE = true
         val binding = KeyBinding("Baton Radial Menu", GLFW.GLFW_KEY_EQUAL, SoundBounds.KEYBIND_CATEGORY)
     }
 }
