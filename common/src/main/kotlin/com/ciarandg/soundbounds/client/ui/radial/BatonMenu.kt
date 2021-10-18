09d212def68e60d2be22d2c27a90e6a3c72c831a
Ciaran De Groot <ciaran.deg@gmail.com>
2021-10-17 18:44:19 -0600

    Define BatonMenu interface

--- common/src/main/kotlin/com/ciarandg/soundbounds/client/ui/radial/BatonMenu.kt
+++ common/src/main/kotlin/com/ciarandg/soundbounds/client/ui/radial/BatonMenu.kt
@@ -2,10 +2,21 @@ package com.ciarandg.soundbounds.client.ui.radial
 
 import com.ciarandg.soundbounds.client.ui.radial.buttons.RedoButton
 import com.ciarandg.soundbounds.client.ui.radial.buttons.UndoButton
+import net.minecraft.util.math.Vec2f
 
-object BatonMenu {
-    val commitModeFolder = RadialFolder(listOf(), 0.0f, 0.0f)
-    val selectionModeFolder = RadialFolder(listOf(), 0.0f, 0.0f)
-    val undoButton = UndoButton()
-    val redoButton = RedoButton()
+class BatonMenu(private val origin: Vec2f) {
+    private val commitModeFolder = RadialFolder(listOf(), 0.0f, 0.0f)
+    private val selectionModeFolder = RadialFolder(listOf(), 0.0f, 0.0f)
+    private val undoButton = UndoButton()
+    private val redoButton = RedoButton()
+
+    fun click(mousePos: Vec2f) {
+        getHoveredButton(origin, mousePos).onClick()
+    }
+
+    fun draw() {}
+
+    private fun getHoveredButton(origin: Vec2f, mousePos: Vec2f): RadialButton {
+        TODO()
+    }
 }
