e6138e0ca8165619db7fe92e38daabd9fa2f7233
Ciaran De Groot <ciaran.deg@gmail.com>
2021-10-18 15:30:09 -0600

    Move polar coord to separate class

--- common/src/main/kotlin/com/ciarandg/soundbounds/client/ui/radial/BatonMenuScreen.kt
+++ common/src/main/kotlin/com/ciarandg/soundbounds/client/ui/radial/BatonMenuScreen.kt
@@ -13,11 +13,7 @@ import net.minecraft.text.LiteralText
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.Vec2f
 import org.lwjgl.glfw.GLFW
-import kotlin.math.atan2
-import kotlin.math.cos
-import kotlin.math.hypot
 import kotlin.math.min
-import kotlin.math.sin
 
 class BatonMenuScreen : Screen(LiteralText("Bounds Baton Menu")) {
     private val origin = client?.mouse?.let { Vec2f(it.x.toFloat(), it.y.toFloat()) } ?: Vec2f(0.0f, 0.0f)
@@ -31,7 +27,7 @@ class BatonMenuScreen : Screen(LiteralText("Bounds Baton Menu")) {
         super.render(matrices, mouseX, mouseY, delta)
         renderMenu()
         if (SHOW_DEBUG_LINE) renderDebugLine(mouseX, mouseY)
-        val polar = PolarCoord.fromScreenCoords(mouseX, mouseY, width, height)
+        val polar = PolarCoordinate.fromScreenCoords(mouseX, mouseY, width, height)
         SoundBounds.LOGGER.info("RADIUS: ${polar.radius} ANGLE: ${polar.angle}")
     }
 
@@ -62,27 +58,6 @@ class BatonMenuScreen : Screen(LiteralText("Bounds Baton Menu")) {
             drawVertex(minX, minY, Vec2f(0.0f, 0.0f))
             draw()
         }
-
-    private data class PolarCoord(val radius: Double, val angle: Double) {
-        fun toCartesian(): Pair<Double, Double> = Pair(radius * cos(angle), radius * sin(angle))
-
-        companion object {
-            fun fromScreenCoords(mouseX: Int, mouseY: Int, width: Int, height: Int) =
-                fromScreenCoords(mouseX.toDouble(), mouseY.toDouble(), width.toDouble() / 2, height.toDouble() / 2)
-            fun fromScreenCoords(mouseX: Double, mouseY: Double, centerX: Double, centerY: Double) =
-                fromCartesian(mouseX - centerX, centerY - mouseY)
-            fun fromCartesian(x: Double, y: Double) = PolarCoord(radiusFromCartesian(x, y), angleFromCartesian(x, y))
-
-            private fun radiusFromCartesian(x: Double, y: Double) = hypot(x, y).let {
-                when {
-                    it.isNaN() -> maxOf(x, y)
-                    else -> it
-                }
-            }
-
-            private fun angleFromCartesian(x: Double, y: Double) = atan2(x, y)
-        }
-    }
 
     companion object {
         const val SHOW_DEBUG_LINE = true
