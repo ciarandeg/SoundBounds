eaa1041e766a15f596dd9f799ec577781c13a518
Ciaran De Groot <ciaran.deg@gmail.com>
2021-10-17 18:45:32 -0600

    Fix params in stub classes

--- common/src/main/kotlin/com/ciarandg/soundbounds/client/ui/radial/buttons/CommitModeButton.kt
+++ common/src/main/kotlin/com/ciarandg/soundbounds/client/ui/radial/buttons/CommitModeButton.kt
@@ -2,7 +2,5 @@ package com.ciarandg.soundbounds.client.ui.radial.buttons
 
 import com.ciarandg.soundbounds.client.ui.radial.RadialButton
 
-abstract class CommitModeButton(onClick: () -> Unit, startTheta: Float, endTheta: Float) : RadialButton(onClick, startTheta,
-    endTheta
-) {
+abstract class CommitModeButton(onClick: () -> Unit) : RadialButton(onClick) {
 }
--- common/src/main/kotlin/com/ciarandg/soundbounds/client/ui/radial/buttons/SelectionModeButton.kt
+++ common/src/main/kotlin/com/ciarandg/soundbounds/client/ui/radial/buttons/SelectionModeButton.kt
@@ -2,7 +2,5 @@ package com.ciarandg.soundbounds.client.ui.radial.buttons
 
 import com.ciarandg.soundbounds.client.ui.radial.RadialButton
 
-abstract class SelectionModeButton(onClick: () -> Unit, startTheta: Float, endTheta: Float) : RadialButton(onClick, startTheta,
-    endTheta
-) {
+abstract class SelectionModeButton(onClick: () -> Unit) : RadialButton(onClick) {
 }
