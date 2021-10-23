39811f5dd50e85f6d71885a78563ac5bed6fc8fb
Ciaran De Groot <ciaran.deg@gmail.com>
2021-10-23 03:45:12 -0600

    Gradle: add junit to common project

---
+++ common/src/main/kotlin/com/ciarandg/soundbounds/client/regions/blocktree/BlockTreeTest.kt
@@ -0,0 +1,52 @@
+package com.ciarandg.soundbounds.client.regions.blocktree
+
+import org.junit.jupiter.api.*
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class BlockTreeTest {
+
+    @Test
+    fun getSize() {
+    }
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun addAll() {
+    }
+
+    @Test
+    fun clear() {
+    }
+
+    @Test
+    operator fun iterator() {
+    }
+
+    @Test
+    fun remove() {
+    }
+
+    @Test
+    fun removeAll() {
+    }
+
+    @Test
+    fun retainAll() {
+    }
+
+    @Test
+    fun contains() {
+    }
+
+    @Test
+    fun containsAll() {
+    }
+
+    @Test
+    fun isEmpty() {
+    }
+}
