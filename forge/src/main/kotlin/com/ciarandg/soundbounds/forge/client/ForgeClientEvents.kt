6cddc7564594355f8ad87b29ec8a8796e3de0e7c
Ciaran De Groot <ciaran.deg@gmail.com>
2022-11-23 21:50:05 -0700

    Fix renamed references

--- forge/src/main/kotlin/com/ciarandg/soundbounds/forge/client/ForgeClientEvents.kt
+++ forge/src/main/kotlin/com/ciarandg/soundbounds/forge/client/ForgeClientEvents.kt
@@ -10,8 +10,8 @@ import dev.architectury.utils.GameInstance
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.screen.option.SoundOptionsScreen
 import net.minecraft.sound.SoundCategory
-import net.minecraftforge.client.event.GuiScreenEvent
-import net.minecraftforge.client.event.RenderWorldLastEvent
+import net.minecraftforge.client.event.RenderLevelLastEvent
+import net.minecraftforge.client.event.ScreenEvent
 import net.minecraftforge.client.event.sound.PlaySoundEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 
@@ -20,22 +20,22 @@ class ForgeClientEvents {
     fun disableVanillaMusic(event: PlaySoundEvent) {
         // menu music doesn't trigger PlaySoundEvent for some reason, but it works in-game
         if (event.sound.category == SoundCategory.MUSIC)
-            event.resultSound = null
+            event.sound = null
     }
 
     @SubscribeEvent
-    fun updateGameMusicVolume(event: GuiScreenEvent) {
+    fun updateGameMusicVolume(event: ScreenEvent) {
         if (GameInstance.getClient().currentScreen is SoundOptionsScreen)
             GameMusicVolume.update()
     }
 
     @SubscribeEvent
-    fun render(event: RenderWorldLastEvent) {
+    fun render(event: RenderLevelLastEvent) {
         val player = MinecraftClient.getInstance().player ?: return
         if (!player.itemsHand.any { it.item is Baton }) return
 
         // Offset by camera position, since it is reset by RenderWorldLastEvent
-        val matrixStack = event.matrixStack
+        val matrixStack = event.poseStack
         val cameraPos = MinecraftClient.getInstance().gameRenderer.camera.pos
         matrixStack.translate(-cameraPos.x, -cameraPos.y, -cameraPos.z)
 
