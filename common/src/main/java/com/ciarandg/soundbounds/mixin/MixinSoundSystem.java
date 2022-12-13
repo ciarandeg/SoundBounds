5e6b67eb31a1e72b8e39fac8582443403ae1249c
Ciaran De Groot <ciaran.deg@gmail.com>
2022-12-12 21:39:04 -0700

    Add SoundSystem mixin for cancelling vanilla music playback

--- common/src/main/java/com/ciarandg/soundbounds/mixin/MixinSoundSystem.java
+++ common/src/main/java/com/ciarandg/soundbounds/mixin/MixinSoundSystem.java
@@ -1,4 +1,23 @@
 package com.ciarandg.soundbounds.mixin;
 
+import com.ciarandg.soundbounds.SoundBounds;
+
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.client.sound.SoundSystem;
+import net.minecraft.sound.SoundCategory;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SoundSystem.class)
 public class MixinSoundSystem {
+    @Inject(at = @At("HEAD"), method = "play", cancellable = true)
+    private void onPlay(SoundInstance sound, CallbackInfo info) {
+        if (sound.getCategory() == SoundCategory.MUSIC) {
+            SoundBounds.Companion.getLOGGER().info("Cancelling vanilla Minecraft music");
+            info.cancel();
+        }
+    }
 }
