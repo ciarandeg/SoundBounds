2285f625dfc50b6400b1e9dd53847e555b23b939
Ciaran De Groot <ciaran.deg@gmail.com>
2022-12-12 18:24:25 -0700

    Comment out render event

--- forge/src/main/kotlin/com/ciarandg/soundbounds/forge/client/ForgeClientEvents.kt
+++ forge/src/main/kotlin/com/ciarandg/soundbounds/forge/client/ForgeClientEvents.kt
@@ -29,22 +29,22 @@ class ForgeClientEvents {
             GameMusicVolume.update()
     }
 
-    @SubscribeEvent
-    fun render(event: RenderLevelLastEvent) {
-        val player = MinecraftClient.getInstance().player ?: return
-        if (!player.itemsHand.any { it.item is Baton }) return
+    // @SubscribeEvent
+    // fun render(event: RenderLevelLastEvent) {
+    //     val player = MinecraftClient.getInstance().player ?: return
+    //     if (!player.itemsHand.any { it.item is Baton }) return
 
-        // Offset by camera position, since it is reset by RenderWorldLastEvent
-        val matrixStack = event.poseStack
-        val cameraPos = MinecraftClient.getInstance().gameRenderer.camera.pos
-        matrixStack.translate(-cameraPos.x, -cameraPos.y, -cameraPos.z)
+    //     // Offset by camera position, since it is reset by RenderWorldLastEvent
+    //     val matrixStack = event.poseStack
+    //     val cameraPos = MinecraftClient.getInstance().gameRenderer.camera.pos
+    //     matrixStack.translate(-cameraPos.x, -cameraPos.y, -cameraPos.z)
 
-        // Render player's bounds selection
-        MarkerSelectionRenderer.renderPlayerMarkerSelection(matrixStack)
+    //     // Render player's bounds selection
+    //     MarkerSelectionRenderer.renderPlayerMarkerSelection(matrixStack)
 
-        // Render region visualization
-        val visualizationRegion = ClientWorldRegions[ClientPlayerModel.visualizationRegion]
-        if (visualizationRegion == null) ClientPlayerModel.visualizationRegion = null
-        else RegionVisualizationRenderer.renderRegionVisualization(matrixStack, visualizationRegion)
-    }
+    //     // Render region visualization
+    //     val visualizationRegion = ClientWorldRegions[ClientPlayerModel.visualizationRegion]
+    //     if (visualizationRegion == null) ClientPlayerModel.visualizationRegion = null
+    //     else RegionVisualizationRenderer.renderRegionVisualization(matrixStack, visualizationRegion)
+    // }
 }
