a025c0045ca5a51e2b450ba3f536018aee368157
Ciaran De Groot <ciaran.deg@gmail.com>
2022-12-12 23:10:48 -0700

    Fix WorldRegionState persistence

--- common/src/main/kotlin/com/ciarandg/soundbounds/common/regions/WorldRegionState.kt
+++ common/src/main/kotlin/com/ciarandg/soundbounds/common/regions/WorldRegionState.kt
@@ -1,21 +1,25 @@
 package com.ciarandg.soundbounds.common.regions
 
+import com.ciarandg.soundbounds.SoundBounds
+import dev.architectury.utils.GameInstance
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.world.PersistentState
+import java.io.File
 
 class WorldRegionState : PersistentState() {
     private val regions: MutableMap<String, RegionData> = HashMap()
-
-    fun fromTag(tag: NbtCompound) {
-        regions.putAll(tag.keys.associateWith { regionName -> RegionData.fromTag(tag.getCompound(regionName)) })
-    }
 
     override fun writeNbt(tag: NbtCompound?): NbtCompound {
         val newTag = NbtCompound()
         for (regionName in regions.keys)
             newTag.put(regionName, regions[regionName]?.toTag())
         return newTag
+    }
+
+    override fun save(file: File?) {
+        SoundBounds.LOGGER.info("Saving WorldRegionState, ${regions.size} regions total")
+        super.save(file)
     }
 
     fun regionExists(regionName: String) = regions.containsKey(regionName)
@@ -40,12 +44,25 @@ class WorldRegionState : PersistentState() {
     companion object {
         private const val WORLD_REGIONS_KEY = "sb-regions"
 
-        fun getOrCreate(world: ServerWorld): WorldRegionState =
-            world.persistentStateManager.get({ WorldRegionState() }, WORLD_REGIONS_KEY) ?: run {
-                val state = WorldRegionState()
-                set(world, state)
+        private fun fromTag(tag: NbtCompound): WorldRegionState {
+            val state = WorldRegionState()
+            state.regions.putAll(tag.keys.associateWith { regionName -> RegionData.fromTag(tag.getCompound(regionName)) })
+            return state
+        }
+
+        fun getOrCreate(world: ServerWorld): WorldRegionState {
+            return world.persistentStateManager.getOrCreate({ nbt ->
+                val state = fromTag(nbt)
+                SoundBounds.LOGGER.info("Loaded WorldRegionState with ${state.regions.size} regions")
                 state
-            }
+            }, {
+                SoundBounds.LOGGER.info("Creating empty WorldRegionState")
+                WorldRegionState()
+            },
+                WORLD_REGIONS_KEY
+            )
+        }
+
         fun set(world: ServerWorld, state: WorldRegionState) =
             world.persistentStateManager.set(WORLD_REGIONS_KEY, state)
     }
