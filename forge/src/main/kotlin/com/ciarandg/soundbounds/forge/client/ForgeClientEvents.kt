3e2bc084a7f6a2ce6db6de08f16a6a0638871a9b
Ciaran De Groot <ciaran.deg@gmail.com>
2022-12-13 22:56:37 -0700

    Remove redundant event

--- forge/src/main/kotlin/com/ciarandg/soundbounds/forge/client/ForgeClientEvents.kt
+++ forge/src/main/kotlin/com/ciarandg/soundbounds/forge/client/ForgeClientEvents.kt
@@ -1,17 +1,6 @@
 package com.ciarandg.soundbounds.forge.client
 
-import net.minecraft.sound.SoundCategory
-import net.minecraftforge.client.event.sound.PlaySoundEvent
-import net.minecraftforge.eventbus.api.SubscribeEvent
-
 class ForgeClientEvents {
-    @SubscribeEvent
-    fun disableVanillaMusic(event: PlaySoundEvent) {
-        // menu music doesn't trigger PlaySoundEvent for some reason, but it works in-game
-        if (event.sound.category == SoundCategory.MUSIC)
-            event.sound = null
-    }
-
     // @SubscribeEvent
     // fun render(event: RenderLevelLastEvent) {
     //     val player = MinecraftClient.getInstance().player ?: return
